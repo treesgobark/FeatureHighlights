6922cfed59ce1016793e2ffa4baacd973c728953
Logan Petruziello <LPetruziello@foundationsoft.com>
2022-09-21 16:59:04 -0400

    data object manager example

---
+++ FeatureHighlights/DataObjectManager.cs
@@ -0,0 +1,39 @@
+﻿namespace FeatureHighlights;
+
+public class DataObject
+{
+    public Guid Uid { get; } = Guid.NewGuid();
+}
+
+public class DataObjectManager<T> where T : DataObject, new()
+{
+    protected List<T> ObjectList { get; } = new();
+
+    public Guid Create(Func<T, T>? mutatorFunc = null)
+    {
+        T obj = new();
+        if (mutatorFunc != null)
+        {
+            obj = mutatorFunc(obj);
+        }
+        ObjectList.Add(obj);
+        return obj.Uid;
+    }
+
+    public override string ToString()
+    {
+        string?[] strings = ObjectList.Select(obj => obj.ToString()).ToArray();
+        return $"{{{ string.Join(", ", strings) }}}";
+    }
+}
+
+public class Person : DataObject
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"({ Name }, { Age })";
+    }
+}
--- FeatureHighlights/Program.cs
+++ FeatureHighlights/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FeatureHighlights;
+
+DataObjectManager<Person> personManager = new();
+List<Guid> myPersonIds = new();
+
+Func<Person, Person> mutator1 = person =>
+{
+    person.Name = "John";
+    person.Age = 24;
+    return person;
+};
+myPersonIds.Add(personManager.Create(mutator1));
+
+Func<Person, Person> mutator2 = person =>
+{
+    person.Name = "Jack";
+    person.Age = 27;
+    return person;
+};
+myPersonIds.Add(personManager.Create(mutator2));
+
+Console.WriteLine(personManager);
+myPersonIds.ForEach(id => Console.Write(id + " "));
+
+Func<int, int> increment = num => num + 1;
+Console.WriteLine(increment(5));
+increment += increment;
+Console.WriteLine(increment(5));
